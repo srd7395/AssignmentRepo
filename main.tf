727670c54f0cb615943745c762787846cc8eea0d
srd7395 <121183817+srd7395@users.noreply.github.com>
2023-01-30 15:12:14 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,13 +8,13 @@ resource "azurerm_resource_group" "rgsd" {
  }
 
  resource "azurerm_managed_disk" "rgsd1" {
-  lenghth = {datadisk}
-  name = "${local.name_prefix}-disk${count.index}"
+  count = 4
+  name = "${local.name_prefix}-var.diskname[0]${count.index}"
   resource_group_name = azurerm_resource_group.rgsd.name
   location = azurerm_resource_group.rgsd.location
   storage_account_type = var.satype
   create_option        = "Empty"
-  disk_size_gb         = var.disksize1
+  disk_size_gb         = var.disksize
   tags = var.tagging
   
  }
