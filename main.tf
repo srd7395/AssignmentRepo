bb24eb3c75d28a480a47ae5b7f205b39c3c738a3
srd7395 <121183817+srd7395@users.noreply.github.com>
2023-01-30 15:02:13 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,8 @@ resource "azurerm_resource_group" "rgsd" {
  }
 
  resource "azurerm_managed_disk" "rgsd1" {
-  name = "${local.name_prefix}-${var.diskname1}"
+  lenghth = {datadisk}
+  name = "${local.name_prefix}-disk${count.index}"
   resource_group_name = azurerm_resource_group.rgsd.name
   location = azurerm_resource_group.rgsd.location
   storage_account_type = var.satype
@@ -18,35 +19,4 @@ resource "azurerm_resource_group" "rgsd" {
   
  }
 
-  resource "azurerm_managed_disk" "rgsd2" {
-  name = "${local.name_prefix}-${var.diskname2}"
-  resource_group_name = azurerm_resource_group.rgsd.name
-  location = azurerm_resource_group.rgsd.location
-  storage_account_type = var.satype
-  create_option        = "Empty"
-  disk_size_gb         = var.disksize2
-  tags = var.tagging
-  
- }
-
-  resource "azurerm_managed_disk" "rgsd3" {
-  name = "${local.name_prefix}-${var.diskname3}"
-  resource_group_name = azurerm_resource_group.rgsd.name
-  location = azurerm_resource_group.rgsd.location
-  storage_account_type = var.satype
-  create_option        = "Empty"
-  disk_size_gb         = var.disksize3
-  tags = var.tagging
-  
- }
-
-  resource "azurerm_managed_disk" "rgsd4" {
-  name = "${local.name_prefix}-${var.diskname4}"
-  resource_group_name = azurerm_resource_group.rgsd.name
-  location = azurerm_resource_group.rgsd.location
-  storage_account_type = var.satype
-  create_option        = "Empty"
-  disk_size_gb         = var.disksize4
-  tags = var.tagging
-  
- }
+ 
